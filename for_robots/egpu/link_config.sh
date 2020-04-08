c7fc3b7a4693086feed684a0dbf72fd712dbbaa6
Whillikers <aidanswope@gmail.com>
2020-04-08 11:05:55 -0700

    Factor stay-open functionality into its own script

--- for_robots/egpu/link_config.sh
+++ for_robots/egpu/link_config.sh
@@ -2,8 +2,8 @@
 # Link appropriate configs for xorg, systemd, etc
 if lspci | grep -q "VGA.*NVIDIA"; then
     ln -s ${HOME}/bin/for_robots/egpu/egpu.conf /etc/X11/xorg.conf.d/50-egpu.conf
+    ${HOME}/bin/scripts/,stay_open.sh
     rm /etc/X11/xorg.conf.d/50-no-egpu.conf
-    systemd-inhibit --what=handle-lid-switch sleep infinity
 else
     ln -s ${HOME}/bin/for_robots/egpu/no-egpu.conf /etc/X11/xorg.conf.d/50-no-egpu.conf
     rm /etc/X11/xorg.conf.d/50-egpu.conf
