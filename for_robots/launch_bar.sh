e52cc5b94e1ebf3bdbe778fd0c9992a62a7a430e
Whillikers <aidanswope@gmail.com>
2020-04-08 17:59:35 -0700

    Update polybar config location

--- for_robots/launch_bar.sh
+++ for_robots/launch_bar.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-# TODO: fix config locations and names
-CONFIG_LOC=$HOME/.config/polybar/gigavolt
+CONFIG_LOC=$XDG_CONFIG_HOME/polybar/config
 N_DISPLAYS=$(xrandr --query | grep " connected" | wc -l)
 
 # End existing polybar session
@@ -11,22 +10,22 @@ kill $(ps aux | grep '[p]olybar*' | awk '{print $2}') 2> /dev/null
 
 if [[ $N_DISPLAYS -gt 2 ]]; then
     # Three displays -> two external monitors
-    feh --bg-scale ${HOME}/media/img/wallpapers/ssss-main.jpg \
-        ${HOME}/media/img/wallpapers/ssss-bridge-thin.jpg
+    feh --bg-scale $HOME/media/img/wallpapers/ssss-main.jpg \
+        $HOME/media/img/wallpapers/ssss-bridge-thin.jpg
 
-    nohup polybar --config=$CONFIG_LOC gigavolt_horizmonitor &> /dev/null &
-    nohup polybar --config=$CONFIG_LOC gigavolt_vertmonitor &> /dev/null &
+    nohup polybar --config=$CONFIG_LOC horizmonitor &> /dev/null &
+    nohup polybar --config=$CONFIG_LOC vertmonitor &> /dev/null &
 elif [[ $N_DISPLAYS -gt 1 ]]; then
     # Two displays -> one external monitor, plus laptop
     feh --bg-scale $HOME/media/img/wallpapers/ssss-main.jpg \
         $HOME/media/img/wallpapers/ssss-ocean.jpg
 
-    nohup polybar --config=$CONFIG_LOC gigavolt_horizmonitor &> /dev/null &
-    nohup polybar --config=$CONFIG_LOC gigavolt_main &> /dev/null &
+    nohup polybar --config=$CONFIG_LOC horizmonitor &> /dev/null &
+    nohup polybar --config=$CONFIG_LOC main &> /dev/null &
 else
     # One display -> just laptop screen
-    feh --bg-scale ${HOME}/media/img/wallpapers/ssss-ocean-darkened.jpg
-    nohup polybar --config=$CONFIG_LOC gigavolt_main &> /dev/null &
+    feh --bg-scale $HOME/media/img/wallpapers/ssss-ocean-darkened.jpg
+    nohup polybar --config=$CONFIG_LOC main &> /dev/null &
 fi
 
-compton --config ${HOME}/.config/compton.conf -b
+compton --config $HOME/.config/compton.conf -b
