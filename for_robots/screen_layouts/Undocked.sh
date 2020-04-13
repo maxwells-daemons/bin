09dcb2e51e15bc4bb7b9ce6f08388beadd279f54
Whillikers <aidanswope@gmail.com>
2020-04-13 10:49:45 -0700

    Fix laptop screen name in setup scripts

--- for_robots/screen_layouts/Undocked.sh
+++ for_robots/screen_layouts/Undocked.sh
@@ -3,7 +3,7 @@
 WacomPen='Wacom Pen and multitouch sensor Pen stylus'
 WacomFinger='Wacom Pen and multitouch sensor Finger touch'
 WacomEraser='Wacom Pen and multitouch sensor Pen eraser'
-Tablet=eDP-1-1
+Tablet=eDP1
 
 xsetwacom set "$WacomPen" Rotate none
 xsetwacom set "$WacomFinger" Rotate none
@@ -12,8 +12,8 @@ xinput --map-to-output "$WacomPen" "$Tablet"
 xinput --map-to-output "$WacomFinger" "$Tablet"
 xinput --map-to-output "$WacomEraser" "$Tablet"
 
-echo eDP-1-1 > $HOME/.leftmonitor
+echo eDP1 > $HOME/.leftmonitor
 
-xrandr --output VIRTUAL1 --off --output eDP-1-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP1-1 --off --output DP1-2 --off --output DP1-3 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP1-1 --off --output DP1-2 --off --output DP1-3 --off --output DP2 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2 --off
 
 i3-msg restart
