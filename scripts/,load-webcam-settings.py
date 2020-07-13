2db4f0bc9b461be49baae51948e3b47f99df650d
maxwells-daemons <aidanswope@gmail.com>
2020-07-13 00:36:33 -0700

    Add script to load webcam settings

---
+++ scripts/,load-webcam-settings.py
@@ -0,0 +1,27 @@
+#!/bin/python3
+import click
+import subprocess
+
+
+@click.command()
+@click.argument("settings-file", type=click.Path(exists=True, dir_okay=False))
+@click.option(
+    "-d",
+    "--device",
+    type=click.Path(exists=True, dir_okay=False),
+    default="/dev/video0",
+)
+def main(settings_file: str, device: str):
+    with open(settings_file, "r") as f:
+        setting_lines = f.readlines()
+
+    for line in setting_lines:
+        args = line.split()
+        name = args[0]
+        value = args[-1].split("=")[-1]
+
+        subprocess.run(["v4l2-ctl", "-d", device, f"--set-ctrl={name}={value}"])
+
+
+if __name__ == "__main__":
+    main()
