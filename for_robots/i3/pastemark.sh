95ad6ae088f25152cf4b338a2da36b9a2822ce0e
Aidan Swope <aidanswope@gmail.com>
2020-10-16 05:22:02 -0700

    Put webcam settings in ~/.config

--- for_robots/i3/pastemark.sh
+++ for_robots/i3/pastemark.sh
@@ -4,10 +4,10 @@
 i3-msg "mark --add λ"
 i3-msg '[con_mark="a"] focus'
 sleep 0.025
-LINES="$(xclip -o)"
+LINES="$(pasteclip)"
 while IFS= read -r line;
 do
-    xdotool type --clearmodifiers --delay 1 "$line"
+    xdotool type --clearmodifiers --delay 2 "$line"
     xdotool key KP_Enter
 done <<< $LINES
 i3-msg '[con_mark="λ"] focus'
