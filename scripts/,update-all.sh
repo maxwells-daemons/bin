1f2ce52772f54ef40c4a68207bfbc9c912783dcb
Aidan Swope <aidanswope@gmail.com>
2020-11-14 06:07:35 -0800

    Add zsh stuff to update script

--- scripts/,update-all.sh
+++ scripts/,update-all.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 # Update all system packages, python tools, and vim plugins
 
 echo "Updating system packages..."
@@ -8,7 +8,6 @@ echo
 echo "Updating Python packages in tools environment..."
 PYTHON_TOOLS_PIP="${XDG_DATA_HOME}/pyenv/versions/tools/bin/python -m pip"
 PYTHON_TOOLS_OUTDATED=$($PYTHON_TOOLS_PIP list --outdated --format freeze | sed 's/==/>=/')
-echo $PYTHON_TOOLS_OUTDATED
 $PYTHON_TOOLS_PIP install --upgrade pip
 if [ ! -z $PYTHON_TOOLS_OUTDATED ]; then
     $PYTHON_TOOLS_PIP install --upgrade $PYTHON_TOOLS_OUTDATED
@@ -18,5 +17,11 @@ echo
 echo "Updating nvim plugins..."
 nvim -c 'PlugUpgrade' -c 'PlugUpdate' -c 'qa!'
 echo
+
+echo "Updating zsh plugins..."
+source $HOME/.config/shells/zsh/.zshrc
+omz update
+antigen update
+antigen cleanup --force
 
 echo "Done!"
