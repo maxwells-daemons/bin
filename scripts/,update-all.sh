6836a50e0333c05ba22c77fa88b249dbdd3d8b5b
Aidan Swope <aidanswope@gmail.com>
2020-11-27 14:39:16 -0800

    Use pipupdate for updating python packages

--- scripts/,update-all.sh
+++ scripts/,update-all.sh
@@ -6,13 +6,8 @@ yay -Syu
 echo
 
 echo "Updating Python packages in tools environment..."
-TOOLS_PYTHON="${XDG_DATA_HOME}/pyenv/versions/tools/bin/python"
-PYTHON_TOOLS_OUTDATED=$($TOOLS_PYTHON -m pip list --outdated --format freeze | sed 's/==/>=/')
-$TOOLS_PYTHON -m pip install --upgrade pip
-if [ ! -z $PYTHON_TOOLS_OUTDATED ]; then
-    $TOOLS_PYTHON -m pip install --upgrade $PYTHON_TOOLS_OUTDATED
-fi
-echo
+pipupgrade --self -y
+pipupgrade -y
 
 echo "Updating nvim plugins..."
 nvim -c 'PlugUpgrade' -c 'PlugUpdate' -c 'qa!'
