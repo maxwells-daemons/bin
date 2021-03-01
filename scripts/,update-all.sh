0570f38f0f7cfb5af1031ac2fe55a712a4ae47f7
Aidan Swope <aidanswope@gmail.com>
2021-02-28 22:37:53 -0800

    Update pipupgrade and jupyter install in update-all

--- scripts/,update-all.sh
+++ scripts/,update-all.sh
@@ -7,7 +7,7 @@ echo
 
 echo "Updating Python packages in tools environment..."
 pipupgrade --self -y
-pipupgrade -y
+pipupgrade --all -y
 
 echo "Updating nvim plugins..."
 nvim -c 'PlugUpgrade' -c 'PlugUpdate' -c 'qa!'
@@ -18,5 +18,8 @@ source $HOME/.config/shells/zsh/.zshrc
 omz update
 antigen update
 antigen cleanup --force
+
+echo "Updating Jupyter build..."
+sudo jupyter lab build
 
 echo "Done!"
